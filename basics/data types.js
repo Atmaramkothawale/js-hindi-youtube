3bf39b38c6bd54ddcfc8064e99af901b3a689da9
Atmaramkothawale <atmaramkothawale0137@gmail.com>
2024-08-07 10:59:33 +0000

    data types

---
+++ basics/data types.js
@@ -0,0 +1,33 @@
+"use strict";//treat all JS code as newer version 
+
+// alert (3 + 3 ) // we are using node JS 
+
+
+console.log(3 + 3) //code readability should be high
+
+
+console.log(AKSHAY)
+
+let name = "AKSHAY"
+let age = 18 
+let isLoggedIn= false 
+let state;
+
+
+// number=> 2 to power 52/53
+//bigint 
+//string=>""
+//boolean=>true/flase
+//null=>standalone value 
+//undefined=> 
+//symbol=>unique 
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 
+
+
+ 
+
