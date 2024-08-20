db6e3c84c0e307b2aa5c7932d3028212e3bcc481
Atmaramkothawale <atmaramkothawale0137@gmail.com>
2024-08-20 04:28:23 +0000

    conversion of data types

---
+++ basics/dataconver.js
@@ -0,0 +1,34 @@
+let score ="33"
+
+console.log (typeof score);
+console.log (typeof(score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+
+//"33">33
+//"33abc" > NAN
+//true>1; 
+//false>0
+
+
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+
+//1> true;0>False
+//""> Flase
+//"akshay"=>true
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
